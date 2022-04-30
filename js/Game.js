90ee3808740e4e48bef355d267a6bdd8bded8f05
Lucas Baste <kelu33@gmail.com>
2022-04-30 16:28:00 +0200

    tuto chapter 2

--- js/Game.js
+++ js/Game.js
@@ -5,22 +5,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
 Game = function(canvasId) {
     // Canvas et engine défini ici
-    let canvas = document.getElementById(canvasId);
-    let engine = new BABYLON.Engine(canvas, true);
-    let _this = this;
-
+    var canvas = document.getElementById(canvasId);
+    var engine = new BABYLON.Engine(canvas, true);
+    this.engine = engine;
+    var _this = this;
+    _this.actualTime = Date.now();
     // On initie la scène avec une fonction associé à l'objet Game
     this.scene = this._initScene(engine);
+    
+    var _player = new Player(_this, canvas);
 
-    // On apelle Player
-    let _player = new Player(_this, canvas);
-
-    // On apelle Arena
-    let _arena = new Arena(_this);
+    var _arena = new Arena(_this);
 
     // Permet au jeu de tourner
     engine.runRenderLoop(function () {
+
+        // Récuperet le ratio par les fps
+        _this.fps = Math.round(1000/engine.getDeltaTime());
+
+        // Checker le mouvement du joueur en lui envoyant le ratio de déplacement
+        _player._checkMove((_this.fps)/60);
+
         _this.scene.render();
+        
+        // Si launchBullets est a true, on tire
+        if(_player.camera.weapons.launchBullets === true){
+            _player.camera.weapons.launchFire();
+        }
     });
 
     // Ajuste la vue 3D si la fenetre est agrandi ou diminué
@@ -36,8 +47,23 @@ Game = function(canvasId) {
 Game.prototype = {
     // Prototype d'initialisation de la scène
     _initScene : function(engine) {
-        let scene = new BABYLON.Scene(engine);
+        var scene = new BABYLON.Scene(engine);
         scene.clearColor=new BABYLON.Color3(0.9,0.9,0.9);
         return scene;
     }
 };
+
+// ------------------------- TRANSFO DE DEGRES/RADIANS 
+function degToRad(deg)
+{
+   return (Math.PI*deg)/180
+}
+// ----------------------------------------------------
+
+// -------------------------- TRANSFO DE DEGRES/RADIANS 
+function radToDeg(rad)
+{
+   // return (Math.PI*deg)/180
+   return (rad*180)/Math.PI
+}
+// ----------------------------------------------------
